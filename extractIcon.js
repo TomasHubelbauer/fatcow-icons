61164dda0fef95d3e2cdfcd3311a902e8ebc84f6
Tomas Hubelbauer <tomas@hubelbauer.net>
2023-05-15 23:30:44 +0200

    Add more information to the console.log calls
    This will help me filter out the dimensions and thus duplicate entries. Also the stream length will help me understand the decodings.

--- extractIcon.js
+++ extractIcon.js
@@ -14,15 +14,16 @@ export default async function extractIcon(icons, /** @type {string} */ fileName,
 
   // See if `new Response` constructor can be used instead of `UZIP.inflateRaw`
   // TODO: Remove this once tested out
-  console.log(fileName, 'UZIP decoded:', uint8Array.slice(0, 100));
+  console.log(dimension, fileName, 'UZIP decoded:', uint8Array.slice(0, 100), uint8Array.length);
   for (const encoding of ['gzip', 'compress', 'deflate', 'br']) {
     try {
       const response = new Response(arrayBuffer, { headers: { 'Content-Encoding': encoding } });
       const arrayBuffer2 = await response.arrayBuffer();
-      console.log(fileName, encoding, 'decoded:', arrayBuffer2.slice(0, 100), new Uint8Array(arrayBuffer2).slice(0, 100));
+      const uint8Array2 = new Uint8Array(arrayBuffer2);
+      console.log(dimension, fileName, encoding, 'decoded:', uint8Array2.slice(0, 100), uint8Array2.length);
     }
     catch (error) {
-      console.log(fileName, encoding, 'decoding failed', error);
+      console.log(dimension, fileName, encoding, 'decoding failed', error);
     }
   }
 
